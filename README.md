9d86e92ea68a8fc4fd0d9fe5e3219e23edda87bb
Michal Konupcik <michal.konupcik@qinshift.com>
2024-06-12 21:32:45 +0200

    update 1.0.8

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Logo](custom_components/bvk_smartreader/icon.png)
 
-![Version](https://img.shields.io/badge/version-1.0.7-blue)
+![Version](https://img.shields.io/badge/version-1.0.8-blue)
 
 ## How to use
 
--- custom_components/bvk_smartreader/sensor.py
+++ custom_components/bvk_smartreader/sensor.py
@@ -1,10 +1,19 @@
 import subprocess
+import logging
 from homeassistant.helpers.entity import Entity
-
+from homeassistant.helpers.entity_platform import async_get_platforms
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up BVK Smart Reader sensor platform."""
+    # Check if the entity is already added to avoid duplication
+    platforms = async_get_platforms(hass, DOMAIN)
+    if any(platform.platform_name == 'sensor' for platform in platforms):
+        _LOGGER.warning("BVK Smart Reader sensor has already been set up!")
+        return
+
     async_add_entities([WaterMeterSensor(hass.data[DOMAIN])])
 
 class WaterMeterSensor(Entity):
@@ -30,15 +39,22 @@ class WaterMeterSensor(Entity):
         password = self._config["password"]
         script_path = './custom_components/bvk_smartreader/getBvkSuezData.sh'
 
+        _LOGGER.debug("Checking if script is executable: %s", script_path)
+        result = subprocess.run(['ls', '-l', script_path], capture_output=True, text=True)
+        _LOGGER.debug("Script permissions: %s", result.stdout.strip())
+
         try:
+            _LOGGER.debug("Running script %s with username %s", script_path, username)
             result = subprocess.run(
                 [script_path, username, password],
                 capture_output=True,
                 text=True,
                 check=True
             )
+            _LOGGER.debug("Script output: %s", result.stdout.strip())
             self._state = result.stdout.strip()  # Get the output from the script
         except subprocess.CalledProcessError as e:
+            _LOGGER.error("Script error: %s", e)
             self._state = None
             # Log error or handle as needed
             self._state = f"Error: {e}"
