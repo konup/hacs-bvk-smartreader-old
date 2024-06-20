757a97c161ad071d722916afad1751166fe41ebb
Michal Konupcik <michal.konupcik@qinshift.com>
2024-06-20 22:21:26 +0200

    new

--- custom_components/bvk_smartreader/__init__.py
+++ custom_components/bvk_smartreader/__init__.py
@@ -14,6 +14,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
+    # Set execute permission for the script
+    script_path = hass.config.path('custom_components/bvk_smartreader/getBvkSuezData.sh')
+    if os.path.exists(script_path):
+        os.chmod(script_path, 0o755)
+
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
